3c996c996feacb06b901322a83f8e5c5b375309b
siddhartha sankar <coolsiddhu970@gmail.com>
2019-10-20 00:11:09 +0530

    Modified index

---
+++ Angular_Code/src/app/@theme/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-footer',
+  styleUrls: ['./footer.component.scss'],
+  template: `
+    <span class="created-by">Developed by p.siddhartha.sankar</span>
+  `,
+})
+export class FooterComponent {
+}
